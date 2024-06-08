03f9c278d2440f6f53f3fa0e20b88120176f6110
Mo Kawsara <mkwsra@gmail.com>
2024-06-08 15:44:22 +0400

    change activity icon

--- app/Filament/Resources/ActivityResource.php
+++ app/Filament/Resources/ActivityResource.php
@@ -16,7 +16,7 @@ class ActivityResource extends Resource
 {
     protected static ?string $model = Activity::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-trophy';
 
     public static function form(Form $form): Form
     {
