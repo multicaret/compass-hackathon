db63c80a574b9ac3bbeb0ca72a3c36fc5fd98dfc
Mo Kawsara <mkwsra@gmail.com>
2024-06-08 15:50:27 +0400

    add tabs to Activities view

--- app/Filament/Resources/ActivityResource/Pages/ManageActivities.php
+++ app/Filament/Resources/ActivityResource/Pages/ManageActivities.php
@@ -3,12 +3,42 @@
 namespace App\Filament\Resources\ActivityResource\Pages;
 
 use App\Filament\Resources\ActivityResource;
-use Filament\Actions;
+use App\Models\Activity;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ManageRecords;
 
 class ManageActivities extends ManageRecords
 {
     protected static string $resource = ActivityResource::class;
+
+
+    public function getTabs(): array
+    {
+        $physicalCount = Activity::where('type', 'physical')
+                                 ->count();
+
+        $onlineCount = Activity::where('type', 'online')
+                               ->count();
+
+        return [
+            'Physical' => Tab::make('Physical')
+                             ->icon('heroicon-o-building-library')
+                             ->query(fn($query) => $query->where('type', 'physical'))
+                             ->badge($physicalCount),
+            'Online' => Tab::make('Online')
+                           ->icon('heroicon-o-wifi')
+                           ->query(fn($query) => $query->where('type', 'online'))
+                           ->badge($onlineCount),
+            'all' => Tab::make('All')
+                        ->icon('heroicon-o-trophy')
+                        ->badge($physicalCount + $onlineCount),
+        ];
+    }
+
+    public function getDefaultActiveTab(): string|int|null
+    {
+        return 'all';
+    }
 
     protected function getHeaderActions(): array
     {
