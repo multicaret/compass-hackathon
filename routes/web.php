dad5985fc68814bc1b698de7ea2cdb890a47cdbe
Mo Kawsara <mkwsra@gmail.com>
2024-06-09 13:59:11 +0400

    hardcoding the auth and redirect to admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect('admin');
 });
+Route::get('admin/login', function () {
+    Auth::loginUsingId(1);
+
+    return redirect('admin');
+})->name('filament.admin.auth.login');
